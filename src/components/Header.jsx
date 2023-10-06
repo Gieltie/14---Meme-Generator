dbae9292eb8c0aab59b34e43ad20623f3ef2d183
Gael Hofman <Gieltie@outlook.fr>
2023-10-06 23:50:53 +0200

    The end of the project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
                 src={troll}
                 className='header--image' />
             <h2 className='header--title'>Generateur d'image Mimee</h2>
-            <h4 className='header--project'>React Course - Project 3</h4>
+            <h4 className='header--project'>Made by Gael Hofman</h4>
         </header>
     )
 }
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react"
-import memesData from "../data/memesData"
+import { useState, useEffect } from "react"
 
 export default function Meme() {
         const [meme, setMeme] = useState({
@@ -8,12 +7,17 @@ export default function Meme() {
             randomImage: ""
         })
 
-        const [allMemeImages, setAllMimeImages] = useState(memesData)
+        const [allMemes, setAllMemes] = useState([])
+
+        useEffect(() => {
+            fetch("https://api.imgflip.com/get_memes")
+                .then(res => res.json())
+                .then(data => setAllMemes(data.data.memes))
+        }, [])
 
         function getMemeImage() {
-            const memesArray = allMemeImages.data.memes
-            const randomNumber = Math.floor(Math.random() * memesArray.length)
-            const url = memesArray[randomNumber].url
+            const randomNumber = Math.floor(Math.random() * allMemes.length)
+            const url = allMemes[randomNumber].url
             setMeme(prevMeme => ({
                 ...prevMeme,
                 randomImage: url
